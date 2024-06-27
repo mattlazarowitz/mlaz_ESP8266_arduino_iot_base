47597c681a0117bab56f038216e827daab7b3975
Matthew Lazarowitz <mlazarowitz@nvidia.com>
2024-06-27 13:07:03 -0700

    aving current work

--- configObject.hpp
+++ configObject.hpp
@@ -20,14 +20,55 @@ This will enable someone to add config options by updating the list of config
 items rather than have to manually create a new instance of this class manually.
 */
 
+
 class configObject {
 private:
-    String configKey; //JSON config key and form input name.
+    String &configKey; //JSON config key and form input name.
     String configValue;
-    String configPrettyName; //A more 'user friendly' name to display in the HTML
+    //String &configPrettyName; //A more 'user friendly' name to display in the HTML
+    String htmlConfigEntry; //build this once when the class is instantiated. Note that templates can be embedded in templates
+    int inputMaxLength;
  
 public:
 
+    configObject(String &key, String &prettyName, int maxLength) {
+        configKey = key;
+        //inputMaxLength = maxLength;
+        configFieldEncoding = encoding;
+        //Desired output (pseudo code as C++'s string handling is nowhere near as good as more modern languages)
+        //<tr><td>{prettyName} <td><input type="text" name="{key}" maxlength="{maxLength}" placeholder="%{key}%"><br>
+        htmlConfigEntry = String("<tr><td>" + prettyName + 
+            " <td><input type=\"text\" name=\"" + key + 
+            "maxlength=\"" + String(maxLength) + "\"" +
+            "\" placeholder=\"%" + key + "%\"><br>");
+    }
+    
+    configObject(String &key, String &prettyName, int maxLength, JsonDocument &configData) {
+        configObject(key, prettyName, maxLength);
+        //need to check if the key exists and validate the data.
+        if(configData.containsKey(key)) {
+            configValue = configData[key];
+        }
+    }
+    
+    String getConfigField(){
+        return htmlConfigEntry;
+    }
+    
+    String getConfigData(){
+        return configValue;
+    }
+    
+    int setConfigData(String data) {
+        //encoding and size limits should have been taken care of by the browser. 
+        //but this is outside data and should be checked.
+        //Need to check with community about the async webserver's ability to validate character sets for form data.
+        if (data.lenght() > inputMaxLength) {
+            return -1;
+        }
+        configValue = data;
+        reutrn data.lenght();
+    }
 
 
 }
